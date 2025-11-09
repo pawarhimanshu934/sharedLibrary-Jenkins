8bb023dd7c982bcd20a71e376010805b5257681b
pawarhimanshu934 <69887496+pawarhimanshu934@users.noreply.github.com>
2025-11-09 15:47:12 +0530

    Refactor clone function to accept parameters

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,4 +1,4 @@
-def clone(){
-  git url: "https://github.com/pawarhimanshu934/Simple-python-app.git", branch:"main"
-  echo "Code clone success"
+def clone(String url, String branch){
+  git url: "${url}", branch:"${branch}"
+  echo "Code clone success git"
 }
