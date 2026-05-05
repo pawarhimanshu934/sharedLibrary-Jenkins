541f0562cf67f5c08900a0f25e1ac2b2d178b799
pawarhimanshu934 <69887496+pawarhimanshu934@users.noreply.github.com>
2026-05-06 00:28:37 +0530

    Create buildDockerImage.groovy

---
+++ vars/buildDockerImage.groovy
@@ -0,0 +1,13 @@
+def call(Map config = [:]){
+
+  def image_name = config.image_name ?: error("No Image Found")
+  def image_tag = config.image_tag ?: 'latest'
+  def docker_file = config.dockerfile ?: 'Dockerfile'
+  def context = config.context ?: '.'
+
+  echo "Building Docker Image - ${image_name}:${image-tag} using ${docker_file}
+
+  sh """
+  docker build -t ${image_name}:${image_tag} -t ${image_name}:latest -f ${docker_file} ${context}
+  """
+}
