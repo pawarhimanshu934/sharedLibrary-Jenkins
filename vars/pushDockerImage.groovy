c2530471ac06c17cdfa637deabaa8a1790f2f852
pawarhimanshu934 <69887496+pawarhimanshu934@users.noreply.github.com>
2026-05-07 00:52:32 +0530

    Refactor pushDockerImage function for clarity

--- vars/pushDockerImage.groovy
+++ vars/pushDockerImage.groovy
@@ -1,25 +1,25 @@
-def call(Map config = [:]){
+def call(Map config = [:]) {
 
-  image_name = config.image_name ?: error("Image is required")
-  image_tag = config.image_tag ?: 'latest'
-  credentials = config.credentials ?: 'docker-hub-credentials'
+    def image_name = config.image_name ?: error("Image is required")
+    def image_tag = config.image_tag ?: 'latest'
+    def credentials = config.credentials ?: 'docker-hub-credentials'
 
-  echo "Pushing ${image_name}:${image_tag} to Docker Hub"
+    echo "Pushing ${image_name}:${image_tag} to Docker Hub"
 
-  withCredentials([usernamePassword(credentialsId: credentials, 
-                                          passwordVariable: 'DOCKER_PASSWORD', 
-                                          usernameVariable: 'DOCKER_USERNAME')]) {
-            
-            // Login to the registry
-            sh "echo \$DOCKER_PASSWORD | docker login -u \$DOCKER_USERNAME --password-stdin"
-            
-            // Push the image
-            sh "docker push \$DOCKER_USERNAME${image_name}:${image_tag}"
-            sh "docker push \$DOCKER_USERNAME${image_name}:latest"
-            
-            // Logout for security
-            sh "docker logout"  
-  
+    withCredentials([usernamePassword(
+        credentialsId: credentials,
+        passwordVariable: 'DOCKER_PASSWORD',
+        usernameVariable: 'DOCKER_USERNAME'
+    )]) {
+
+        // Login
+        sh "echo \$DOCKER_PASSWORD | docker login -u \$DOCKER_USERNAME --password-stdin"
+
+        // Push
+        sh "docker push \$DOCKER_USERNAME/${image_name}:${image_tag}"
+        sh "docker push \$DOCKER_USERNAME/${image_name}:latest"
+
+        // Logout
+        sh "docker logout"
+    }
 }
-
-  
