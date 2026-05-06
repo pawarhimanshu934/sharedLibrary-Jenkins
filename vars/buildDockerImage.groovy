73ece6cb902af8131d3d6c6632a2b7922c02d3a2
pawarhimanshu934 <69887496+pawarhimanshu934@users.noreply.github.com>
2026-05-06 23:51:17 +0530

    Fix variable reference for image tag in Docker build

--- vars/buildDockerImage.groovy
+++ vars/buildDockerImage.groovy
@@ -5,7 +5,7 @@ def call(Map config = [:]){
   def docker_file = config.dockerfile ?: 'Dockerfile'
   def context = config.context ?: '.'
 
-  echo "Building Docker Image - ${image_name}:${image-tag} using ${docker_file}"
+  echo "Building Docker Image - ${image_name}:${image_tag} using ${docker_file}"
 
   sh """
   docker build -t ${image_name}:${image_tag} -t ${image_name}:latest -f ${docker_file} ${context}
