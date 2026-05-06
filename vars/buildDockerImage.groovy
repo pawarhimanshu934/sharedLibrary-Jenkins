168f2a9554c8966e6a78bb3b829eaecb8350ed65
pawarhimanshu934 <69887496+pawarhimanshu934@users.noreply.github.com>
2026-05-07 01:12:48 +0530

    Update Docker image build and tagging process
    Refactor Docker image build command to separate tagging.

--- vars/buildDockerImage.groovy
+++ vars/buildDockerImage.groovy
@@ -7,7 +7,10 @@ def call(Map config = [:]){
 
   echo "Building Docker Image - ${image_name}:${image_tag} using ${docker_file}"
 
-  sh """
-  docker build -t ${image_name}:${image_tag} -t ${image_name}:latest -f ${docker_file} ${context}
+    sh """
+  docker build -f ${docker_file} ${context} \
+    -t ${image_name}:${image_tag}
+  
+  docker tag ${image_name}:${image_tag} ${image_name}:latest
   """
 }
