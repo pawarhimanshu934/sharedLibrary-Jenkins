9b1bf60565688e3dd02ce2c05d3d8bcb85dc04ec
pawarhimanshu934 <69887496+pawarhimanshu934@users.noreply.github.com>
2026-05-06 22:51:06 +0530

    Add pushDockerImage function for Docker image uploads

---
+++ vars/pushDockerImage.groovy
@@ -0,0 +1,25 @@
+def call(Map config = [:]){
+
+  image_name = config.image_name? : error("Image is required")
+  image_tag = config.image_tag? : 'latest'
+  credentials = config.credentials? : 'docker-hub-credentials'
+
+  echo "Pushing ${image_name}:${image_tag} to Docker Hub"
+
+  withCredentials([usernamePassword(credentialsId: credentials, 
+                                          passwordVariable: 'DOCKER_PASSWORD', 
+                                          usernameVariable: 'DOCKER_USERNAME')]) {
+            
+            // Login to the registry
+            sh "echo \$DOCKER_PASSWORD | docker login -u \$DOCKER_USERNAME --password-stdin"
+            
+            // Push the image
+            sh "docker push \$DOCKER_USERNAME${image_name}:${image_tag}"
+            sh "docker push \$DOCKER_USERNAME${image_name}:latest"
+            
+            // Logout for security
+            sh "docker logout"  
+  
+}
+
+  
