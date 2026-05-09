15bd7f669b69c5935bc85d108cc03bdb477beded
pawarhimanshu934 <69887496+pawarhimanshu934@users.noreply.github.com>
2026-05-10 00:39:01 +0530

    Simplify Docker image push without credentials
    Removed credential handling for Docker push.

--- vars/pushDockerImage.groovy
+++ vars/pushDockerImage.groovy
@@ -2,23 +2,13 @@ def call(Map config = [:]) {
 
     def image_name = config.image_name ?: error("Image is required")
     def image_tag = config.image_tag ?: 'latest'
-    def credentials = config.credentials ?: 'docker-hub-credentials'
 
     echo "Pushing ${image_name}:${image_tag} to Docker Hub"
-
-    withCredentials([usernamePassword(
-        credentialsId: credentials,
-        passwordVariable: 'DOCKER_PASSWORD',
-        usernameVariable: 'DOCKER_USERNAME'
-    )]) {
-
-        sh """
-        echo \$DOCKER_PASSWORD | docker login -u \$DOCKER_USERNAME --password-stdin
-
+    
+    sh """
         docker push ${image_name}:${image_tag}
         docker push ${image_name}:latest
 
         docker logout
-        """
-    }
+    """
 }
